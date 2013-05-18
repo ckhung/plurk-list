7e3c00e355de9a3c4777b4c48c6835afe510828e
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2013-05-18 18:12:23 +0800

    first version

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 plurk-list
 ==========
 
-produces an html list of plurks by processing both the official backup and the rss feed
+English
+-------
+
+These scripts help you generates a list of plurks
+from the official plurk backup or from the plurk rss.
+Currently only Chinese document is available.
+
+中文
+----
+
+從官方備份或 rss 裡產生噗浪發言清單。
+使用方式詳見 [專案官網](http://ckhung.github.com/plurk-list/index.zh_TW.html)。
---
+++ strip.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'QueryPath/QueryPath.php';
+
+foreach (array_slice($argv,1) as $entry) {
+    $qp = htmlqp($entry);
+    preg_match("#(\w+)\.html#", $entry, $url);
+    $url = "http://www.plurk.com/p/" . $url[1];
+    $qp->find(".bigplurk");
+    if ($qp->is("div.user")) {
+	// data is from rss
+	$qp->find("div.content");
+	$qp->remove("div.avatar");
+	$verb = $qp->remove("span.qualifier");
+	$date = $qp->remove("time")->attr("datetime");
+	$qp->remove("div.user");
+	$t = " " . $verb->html() . "\n" . $qp->find("span.plurk_content")->innerHTML();
+    } else {
+	// data is from official plurk backup
+	$qp->remove("a:first");
+	$date = $qp->remove("div.meta")->text();
+	if (preg_match('/private/', $date)) continue;
+	$t = rtrim($qp->innerHTML());
+    }
+    $date = preg_replace(
+	"/(\d{4})-(\d\d)-(\d\d).*?(\d\d):(\d\d):\d\d.*/",
+	"$1-$2-$3 $4:$5", $date
+    );
+    echo "<li><a href='$url'>$date</a> $t</li>\n\n";
+}
+?>
