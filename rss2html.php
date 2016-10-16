b9d838688ea2d44c02f6bd042a0c993e429e97d6
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2016-10-16 16:48:30 +0800

    update to use QueryPath 3.0.5

--- rss2html.php
+++ rss2html.php
@@ -3,7 +3,7 @@
 # wget http://www.plurk.com/ckhung0.xml
 # rss2html.php $(ls *.xml | sort -r)
 
-require_once 'QueryPath/QueryPath.php';
+require_once 'QueryPath/qp.php';
 
 $V = array(
     '分享' => 'shares',
@@ -19,21 +19,21 @@ $V = array(
     '需要' => 'needs',
     '討厭' => 'hates',
     '轉噗' => 'replurks',
+    '转噗' => 'replurks',
     'replurks' => 'replurks',
 );
 
 $last_url = '/p/zzzzzz';
 $last_month = '';
 for ($i=1; $i<count($argv); ++$i) {
-    $qp = htmlqp($argv[$i]);
+    $qp = qp($argv[$i]);
     foreach ($qp->find("entry") as $entry) {
 	$url = $entry->find("link")->attr("href");
 	if ($url < $last_url)
 	    $last_url = $url;
 	else
 	    continue;
-	$date = $entry->end()->find("published");
-	$date = $date->text();
+	$date = $entry->find("published")->text();
 	preg_match('/(\d+)-(\d+)-(\d+).*(\d+)(:\d+)/', $date, $m);
 	$date = sprintf("$m[1]-$m[2]-$m[3] %02d$m[5]", $m[4]);
 	if ($m[2] != $last_month) {
@@ -47,17 +47,23 @@ for ($i=1; $i<count($argv); ++$i) {
 EOF;
 	    $last_month = $m[2];
 	}
-	$content = $entry->end()->find("content");
-	$content = $content->text();
+	$content = $entry->find("content")->text();
 	preg_match('/^(\w+) (\S+) (.*)/', $content, $m);
 	if (isset($V[$m[2]])) {
 	    $verb = $V[$m[2]];
 	    if ($m[2] == 'replurks') $m[2] = '轉噗';
+	    $verb = "<span class='qualifier q_$verb'>$m[2]</span>\n";
+	    $content = $m[3];
 	} else {
-	    echo ">>> '$m[2]' is an unknown verb\n";
+	    if (strlen($m[2]) <= 9) {
+		fwrite(STDERR, "error: '$m[2]' is an unknown verb\n");
+	    } else {
+		fwrite(STDERR, "warning: $url does not have a verb\n");
+	    }
+	    $verb = "";
+	    $content = "$m[2] $m[3]";
 	}
-	$content = "<span class='qualifier q_$verb'>$m[2]</span>\n$m[3]";
-	echo "<li><a href='http://www.plurk.com$url'>$date</a>  $content</li>\n\n";
+	echo "<li><a href='http://www.plurk.com$url'>$date</a>  $verb$content</li>\n\n";
     }
 }
 
